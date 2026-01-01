47d208ef950661c9f23e8c9957eeae9d769219a8
Nurul Huda (Apon) <me@nurulhudaapon.com>
2026-01-01 13:28:24 +0600

    feat: add sqlite as lib

--- build.zig
+++ build.zig
@@ -54,8 +54,28 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     });
 
+    mcp_exe.addCSourceFile(.{
+        .file = b.path("lib/sqlite/sqlite3.c"),
+        .flags = &[_][]const u8{
+            "-DSQLITE_DQS=0",
+            "-DSQLITE_DEFAULT_WAL_SYNCHRONOUS=1",
+            "-DSQLITE_USE_ALLOCA=1",
+            "-DSQLITE_THREADSAFE=1",
+            "-DSQLITE_TEMP_STORE=3",
+            "-DSQLITE_ENABLE_API_ARMOR=1",
+            "-DSQLITE_ENABLE_UNLOCK_NOTIFY",
+            "-DSQLITE_DEFAULT_FILE_PERMISSIONS=0600",
+            "-DSQLITE_OMIT_DECLTYPE=1",
+            "-DSQLITE_OMIT_DEPRECATED=1",
+            "-DSQLITE_OMIT_LOAD_EXTENSION=1",
+            "-DSQLITE_OMIT_PROGRESS_CALLBACK=1",
+            "-DSQLITE_OMIT_SHARED_CACHE",
+            "-DSQLITE_OMIT_TRACE=1",
+            "-DSQLITE_OMIT_UTF16=1",
+            "-DHAVE_USLEEP=0",
+        },
+    });
     mcp_exe.linkLibC();
-    mcp_exe.linkSystemLibrary("sqlite3");
     mcp_exe.root_module.addImport("zqlite", zqlite.module("zqlite"));
 
     b.installArtifact(mcp_exe);
