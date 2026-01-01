91c801d63e04764680ba554f2417f4cd79f6d115
Nurul Huda (Apon) <me@nurulhudaapon.com>
2026-01-01 13:26:08 +0600

    feat: setup zqlite example

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const zqlite = @import("zqlite");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -8,4 +9,28 @@ pub fn main() !void {
     _ = allocator;
 
     std.debug.print("Hello, World!\n", .{});
+
+    // good idea to pass EXResCode to get extended result codes (more detailed error codes)
+    const flags = zqlite.OpenFlags.Create | zqlite.OpenFlags.EXResCode;
+    var conn = try zqlite.open(".zig-cache/tmp/test.sqlite", flags);
+    defer conn.close();
+
+    try conn.exec("create table if not exists test (name text)", .{});
+    try conn.exec("insert into test (name) values (?1), (?2)", .{ "Leto", "Ghanima" });
+
+    {
+        if (try conn.row("select * from test order by name limit 1", .{})) |row| {
+            defer row.deinit();
+            std.debug.print("name: {s}\n", .{row.text(0)});
+        }
+    }
+
+    {
+        var rows = try conn.rows("select * from test order by name", .{});
+        defer rows.deinit();
+        while (rows.next()) |row| {
+            std.debug.print("name: {s}\n", .{row.text(0)});
+        }
+        if (rows.err) |err| return err;
+    }
 }
